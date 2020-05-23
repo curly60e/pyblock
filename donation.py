5558ecb49d6714caf24f39d2b10b504ecf01c355
curly60e <55191248+curly60e@users.noreply.github.com>
2020-05-23 19:54:41 -0300

    Update for v.0.1.0
    Added Tester donation QRCode

--- donation.py
+++ donation.py
@@ -2,8 +2,6 @@ import requests
 import qrcode
 
 
-
-
 def donationPN():
     qr = qrcode.QRCode(
     version=1,
@@ -36,6 +34,41 @@ def donationLN():
     border=4,
     )
     url = 'https://api.tippin.me/v1/public/addinvoice/royalfield370'
+    response = requests.get(url)
+    responseB = str(response.text)
+    responseC = responseB
+    lnreq = responseC.split(',')
+    lnbc1 = lnreq[1]
+    lnbc1S = str(lnbc1)
+    lnbc1R = lnbc1S.split(':')
+    lnbc1W = lnbc1R[1]
+    ln = str(lnbc1W)
+    ln1 = ln.strip('"')
+    qr.add_data(ln1)
+    qr.print_ascii()
+    print("LND Invoice: " + ln1)
+
+    
+def donationAddrTst():
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    url = 'bc1qwtzwu2evtchkvnf3ey6520yprsyv7vrjvhula5'
+    qr.add_data(url)
+    qr.print_ascii()
+    print("Bitcoin Address Bech32: " + url)
+    
+def donationLNTst():
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    url = 'https://api.tippin.me/v1/public/addinvoice/__B__T__C__'
     response = requests.get(url)
     responseB = str(response.text)
     responseC = responseB
