936f8f40fe897e58d61c9338404be885091a4954
curly60e <55191248+curly60e@users.noreply.github.com>
2020-05-24 15:37:12 -0300

    Update clone.py

--- clone.py
+++ clone.py
@@ -4,19 +4,19 @@ import time as t
 
 
 def gitclone():
-    if os.path.isdir('$HOME/python-blockclock/satellite'):
+    if os.path.isdir('$HOME/pyblock/satellite'):
         print("OK Pass")
         
     else:
         url = "https://github.com/curly60e/satellite"
         os.system("git clone " + url)
-        os.system("mkdir $HOME/python-blockclock/satellite/api/examples/.gnupg")
-        os.system("cp -r $HOME/python-blockclock/satellite/api/examples $HOME/python-blockclock/")
-        os.system("gpg --full-generate-key --homedir $HOME/python-blockclock/examples/.gnupg")
+        os.system("mkdir $HOME/pyblock/satellite/api/examples/.gnupg")
+        os.system("cp -r $HOME/pyblock/satellite/api/examples $HOME/python-blockclock/")
+        os.system("gpg --full-generate-key --homedir $HOME/pyblock/examples/.gnupg")
         
 def satnode():
-    os.system("python3 $HOME/python-blockclock/examples/demo-rx.py &")
+    os.system("python3 $HOME/pyblock/examples/demo-rx.py &")
     t.sleep(5)
-    os.system("python3 $HOME/python-blockclock/examples/api_data_reader.py --demo  --plaintext ")
+    os.system("python3 $HOME/pyblock/examples/api_data_reader.py --demo  --plaintext ")
     
 
