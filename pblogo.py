0d73dbdfeea0408250a89e2de87621babdaca6d1
curly60e <55191248+curly60e@users.noreply.github.com>
2020-05-29 18:06:52 -0300

    Payment confirmation art

--- pblogo.py
+++ pblogo.py
@@ -7,3 +7,41 @@ def blogo():
 ██║        ██║   ██████╔╝███████╗╚██████╔╝╚██████╗██║  ██╗
 ╚═╝        ╚═╝   ╚═════╝ ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝                                                         
 """)
+    
+def tick():
+    print("""
+\t\t                                                                           .-:/::.
+\t\t                                                                         -/+oooooo/
+\t\t                                                                      .:+oooooooooo.
+\t\t                                                                    -+oooooooooooo+``
+\t\t                      .--------------------------------------.   .:+oooooooooooo+/.`
+\t\t                   ./+ooooooooooooooooooooooooooooooooooooo+-``.:+ooooooooooo+/-`
+\t\t                  /oooooooooooooooooooooooooooooooooooooo+:``./oooooooooooo+-.`
+\t\t                 /oooooo++++++++++++++++++++++++++++++++:``.:oooooooooooo/.`
+\t\t                .oooooo-``````````````````````````````````:+oooooooooo+:.`
+\t\t                -oooooo`````````````````````````````````-+oooooooooo+:.``
+\t\t                -oooooo```````````````````````````````./ooooooooooo:```.`
+\t\t                -oooooo``````````````````````````````-ooooooooooo/.``-+/`
+\t\t                -oooooo``````-/+++:.```````````````./oooooooooo+.``.+oo/`
+\t\t                -oooooo`````+ooooooo/`````````````-ooooooooooo:```/oooo/`
+\t\t                -oooooo````-ooooooooo+-``````````/oooooooooo/.```/ooooo/`
+\t\t                -oooooo`````+oooooooooo/```````.+oooooooooo-`````/ooooo/`
+\t\t                -oooooo``````:oooooooooo+-````-oooooooooo+.``````/ooooo/`
+\t\t                -oooooo```````.+oooooooooo/``:oooooooooo:````````/ooooo/`
+\t\t                -oooooo`````````:oooooooooo+/oooooooooo-`````````/ooooo/`
+\t\t                -oooooo``````````.+oooooooooooooooooo+.``````````/ooooo/`
+\t\t                -oooooo````````````:oooooooooooooooo+````````````/ooooo/`
+\t\t                -oooooo`````````````.+ooooooooooooo/`````````````/ooooo/`
+\t\t                -oooooo```````````````:ooooooooooo/``````````````/ooooo/`
+\t\t                -oooooo````````````````.+oooooooo/```````````````/ooooo/`
+\t\t                -oooooo``````````````````:+oooo+/````````````````/ooooo/`
+\t\t                -oooooo````````````````````.--.``````````````````/ooooo/`
+\t\t                -oooooo``````````````````````````````````````````/ooooo/`
+\t\t                -oooooo``````````````````````````````````````````/ooooo/`
+\t\t                -oooooo.`````````````````````````````````````````+ooooo/`
+\t\t                `+ooooo+/::::::::::::::::::::::::::::::::::::::/+oooooo-`
+\t\t                 .+ooooooooooooooooooooooooooooooooooooooooooooooooooo:`
+\t\t                  `:+oooooooooooooooooooooooooooooooooooooooooooooo+/.`
+\t\t                    `.-:////////////////////////////////////////::.``
+\t\t                     `````````````````````````````````````````````""")
+
