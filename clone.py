6e36f40b4cd7dfdfdb99433d79e7737ddcff00c1
curly60e <55191248+curly60e@users.noreply.github.com>
2020-06-03 11:10:12 -0300

    Improved code
    Better satnode clone.
    $HOME folder changed to satellite folder inside pyblock.

--- clone.py
+++ clone.py
@@ -1,28 +1,20 @@
-#Developer: Curly60e
-#PyBLOCK its a clock of the Bitcoin blockchain.
-#Version: 0.3.0
-
 import os
 import os.path
 import time as t
 
 
 def gitclone():
-    if os.path.isdir('$HOME/pyblock/satellite'):
-        print("OK Pass")
-        
-    else:
-        url = "https://github.com/curly60e/satellite"
-        os.system("git clone " + url)
-        os.system("mkdir $HOME/pyblock/satellite/api/examples/.gnupg")
-        os.system("cp -r $HOME/pyblock/satellite/api/examples $HOME/pyblock/")
-        os.system("gpg --full-generate-key --homedir $HOME/pyblock/examples/.gnupg")
+    url = "https://github.com/curly60e/satellite"
+    os.system("git clone " + url)
+    os.system("mkdir satellite/api/examples/.gnupg")
+    #os.system("cp -r $HOME/pyblock/satellite/api/examples $HOME/pyblock/")
+    os.system("gpg --full-generate-key --homedir satellite/api/examples/.gnupg")
         
 def satnode():
     try:
-        os.system("python3 $HOME/pyblock/examples/demo-rx.py &")
+        os.system("python3 satellite/api/examples/demo-rx.py &")
         t.sleep(5)
-        os.system("python3 $HOME/pyblock/examples/api_data_reader.py --demo  --plaintext ")
+        os.system("python3 satellite/api/examples/api_data_reader.py --demo  --plaintext ")
     except (KeyboardInterrupt, SystemExit):
         os.system("ps -ef | grep demo-rx.py | grep -v grep | awk '{print $2}' | xargs kill -9")
     
