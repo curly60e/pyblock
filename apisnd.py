90a9c2c3dd0235e0cd64a8e2598c4bbc58e110a7
curly60e <55191248+curly60e@users.noreply.github.com>
2020-06-05 17:00:44 -0300

    Bug fix
    Fix on line 20, the embeded, has not space, added a space.

--- apisnd.py
+++ apisnd.py
@@ -17,7 +17,7 @@ def apisender():
     )
     url = 'https://api.blockstream.space/order'
     message = input("\nInsert your Message: ")
-    sentby = "- PyBLOCK."
+    sentby = " - PyBLOCK."
     print("ATENTION: Minimum amount for sending a text is 5000 MSats")
     amountmsat = input("\nInsert the amount in MSats: ")
     curl = 'curl -F ' "bid={} ".format(amountmsat) + '-F ' + ' "message=' + message + sentby + '" ' + url
