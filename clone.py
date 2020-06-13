e7da529cfb7c3457fb17e3208a101318148fd770
curly60e <55191248+curly60e@users.noreply.github.com>
2020-06-13 20:58:40 -0300

    Bug fix when closing
    Same issue on feed.py

--- clone.py
+++ clone.py
@@ -12,20 +12,20 @@ def gitclone():
     os.system("git clone " + url)
     os.system("mkdir satellite/api/examples/.gnupg")
     os.system("gpg --full-generate-key --homedir satellite/api/examples/.gnupg")
-        
+
 def satnode():
     try:
         os.system("python3 satellite/api/examples/demo-rx.py &")
         t.sleep(5)
         os.system("python3 satellite/api/examples/api_data_reader.py --demo  --plaintext ")
-    except (KeyboardInterrupt, SystemExit):
+    except:
+        os.system("ps -ef | grep api_data_reader.py | grep -v grep | awk '{print $2}' | xargs kill -9")
         os.system("ps -ef | grep demo-rx.py | grep -v grep | awk '{print $2}' | xargs kill -9")
-    
+
 def matrixsc():
     if os.path.isdir('$HOME/pyblock/terminal_matrix'):
         print("OK Pass")
-        
+
     else:
         url = "https://github.com/curly60e/terminal_matrix.git"
         os.system("git clone " + url)
-
