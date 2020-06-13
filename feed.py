cd7640e591020fb2c52d69b6ae9489323264aae9
curly60e <55191248+curly60e@users.noreply.github.com>
2020-06-13 20:52:04 -0300

    Bug Fix when closing
    When apply Control+C it should kill both process api_data_reader.py y demo-rx.py.

--- feed.py
+++ feed.py
@@ -9,18 +9,16 @@ import time as t
 
 def readFile():
     try:
+        print ("\n\033[1;34;40mWaiting for new data...\n")
         while True:
             downloadsFolder = 'downloads/'
             if not os.listdir(downloadsFolder):
-                print ("\n\033[1;34;40mWaiting for new data...\n")
-                t.sleep(50)
+                continue
             else:
                 print("\t\t\n\033[1;33;40mNew message from Space just arrived...\033[0;37;40m\n")
                 os.system("cat downloads/*")
                 os.system("rm downloads/*")
-                
-    except (KeyboardInterrupt, SystemExit):
-        os.system("ps -ef | grep api_data_reader.py | grep -v grep | awk '{print $2}' | xargs kill -9")
-        
 
-        
+    except:
+        os.system("ps -ef | grep api_data_reader.py | grep -v grep | awk '{print $2}' | xargs kill -9")
+        os.system("ps -ef | grep demo-rx.py | grep -v grep | awk '{print $2}' | xargs kill -9")
