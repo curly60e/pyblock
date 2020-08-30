1e47dfab54338d8cf7a9c6a9e1d4624b2152b095
curly60e <55191248+curly60e@users.noreply.github.com>
2020-08-30 12:03:50 -0300

    Connect, List and Disconnect peers
    Added List peers, inside it shows details from a selected peer and ask you if the user want to disconnect to that peer. 
    Added Connect peer, just adding PubKey@IP:Port it will connect on the local mode.

--- nodeconnection.py
+++ nodeconnection.py
@@ -110,6 +110,82 @@ def consoleLN(): # get into the console from bitcoin-cli
         print(lsd1)
         lsd.close()
 
+def locallistpeers():
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    while True:
+        clear()
+        print("\033[1;32;40m")
+        blogo()
+        print("\033[0;37;40m")
+        print("<<< Back to the Main Menu Press Control + C.\n\n")
+        lncli = " listpeers"
+        lsd = os.popen(lndconnectload['ln'] + lncli).read()
+        lsd0 = str(lsd)
+        d = json.loads(lsd0)
+        n = d['peers']
+        try:
+            print("\n\tLIST PEERS\n")
+            for item_ in n:
+                s = item_
+
+                print("PubKey: " + s['pub_key'] + " @" + s['address'])
+            nd = input("\nSelect PubKey: ")
+            for item in n:
+                s = item
+                nn = s['pub_key']
+                if nd == nn:
+                    print("\n----------------------------------------------------------------------------------------------------")
+                    print("""
+                        PEER DECODED\n
+                        Bytes Sent: {}
+                        Bytes Recv: {}
+                        Sat Sent: {} sats
+                        Sat Recv: {} sats
+                    """.format(s['bytes_sent'], s['bytes_recv'], s['sat_sent'], s['sat_recv']))
+                    print("-----------------------------------------------------------------------------------------------------\n")
+                    print("\n\tPeer: " + nd)
+                    print("\033[1;30;47m")
+                    qr.add_data(s['pub_key'])
+                    qr.print_ascii()
+                    print("\033[0;37;40m")
+                    qr.clear()
+
+            pp = input("\nDo you want to disconnect? Y/n: ")
+            if pp in ["Y", "y"]:
+                lncli = " disconnect"
+                lsd = os.popen(lndconnectload['ln'] + lncli + " " + nd).read()
+                lsd0 = str(lsd)
+                d = json.loads(lsd0)
+                print("\n\tDisconnected from peer " + nd)
+                input("\nContinue... ")
+            elif pp in ["N", "n"]:
+                input("\nContinue... ")
+        except:
+            break
+
+def localconnectpeer():
+    try:
+        clear()
+        print("\033[1;32;40m")
+        blogo()
+        print("\033[0;37;40m")
+        print("<<< Back to the Main Menu Press Control + C.\n\n")
+        print("\n\tCONNECT TO NEW PEER\n")
+        a = input("Insert PeerID@IP:PORT: ")
+        lncli = " connect "
+        lsd = os.popen(lndconnectload['ln'] + lncli + a).read()
+        lsd0 = str(lsd)
+        print(lsd0)
+        input("\nContinue... ")
+    except:
+        pass
+localconnectpeer()
+
 def locallistchaintxns():
     qr = qrcode.QRCode(
     version=1,
