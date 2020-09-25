b2a7e8aa8da847333cd541a594a8533599663733
curly60e <55191248+curly60e@users.noreply.github.com>
2020-09-25 20:24:58 -0300

    Update PyBlock.py

--- PyBlock.py
+++ PyBlock.py
@@ -214,7 +214,7 @@ def countdownblock():
             block = os.popen(str(bitcoinclient)).read() # 'getblockcount' convert to string
             b = block
             if a == b:
-                print("Congratulations, you achieve block " + a)
+                print("#RuningTheNumbers " + a + "PyBLOCK")
                 input("\nContinue...")
                 break
             elif n != int(b):
