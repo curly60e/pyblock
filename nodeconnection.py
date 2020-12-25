ad2e553476ec1bd96a37ba0b19cfd5b66299d122
curly60e <55191248+curly60e@users.noreply.github.com>
2020-12-25 08:46:21 -0300

    Remote Bclock improvements

--- nodeconnection.py
+++ nodeconnection.py
@@ -90,31 +90,29 @@ def remoteHalving():
 
 
 def remotegetblock():
+    if os.path.isfile('pyblocksettingsClock.conf') or os.path.isfile('pyblocksettingsClock.conf'): # Check if the file 'bclock.conf' is in the same folder
+        settingsv = pickle.load(open("pyblocksettingsClock.conf", "rb")) # Load the file 'bclock.conf'
+        settingsClock = settingsv # Copy the variable pathv to 'path'
+    else:
+        settingsClock = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
+        pickle.dump(settingsClock, open("pyblocksettingsClock.conf", "wb"))
     b = rpc('getblockcount')
     c = str(b)
     a = c
+    output = render(str(c), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
+    print(output)
     while True:
-        if os.path.isfile('pyblocksettingsClock.conf') or os.path.isfile('pyblocksettingsClock.conf'): # Check if the file 'bclock.conf' is in the same folder
-            settingsv = pickle.load(open("pyblocksettingsClock.conf", "rb")) # Load the file 'bclock.conf'
-            settingsClock = settingsv # Copy the variable pathv to 'path'
-        else:
-            settingsClock = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
-            pickle.dump(settingsClock, open("pyblocksettingsClock.conf", "wb"))
+        x = a
         b = rpc('getblockcount')
         c = str(b)
         if c > a:
-            output = render(str(c), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
-            print("\a" + output)
-            t.sleep(10)
-            break
-        elif c == a:
-            output = render(str(c), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
-            print(output)
-            t.sleep(10)
             clear()
             closed()
-        else:
-            break
+            output = render(str(c), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
+            print("\a" + output)
+            a = c
+        elif x == a:
+            pass
 
 def remotegetblockcount(): # get access to bitcoin-cli with the command getblockcount
     while True:
