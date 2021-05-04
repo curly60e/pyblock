ed9bed2946b9cc057964a70ae3b08dbab22dd579
curly60e <55191248+curly60e@users.noreply.github.com>
2021-05-03 21:02:10 -0300

    Add files via upload

---
+++ taproot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+BLOCKCHAININFO=$(bitcoin-cli getblockchaininfo)
+TAPROOT=$(echo "$BLOCKCHAININFO" | jq .softforks.taproot.bip9)
+SINCE=$(echo "$TAPROOT" | jq .since)
+ELAPSED=$(echo "$TAPROOT" | jq .statistics.elapsed)
+for BLOCK in $(seq "$SINCE" $((SINCE + ELAPSED - 1))); do
+  HASH=$(bitcoin-cli getblockhash "$BLOCK")
+  HEADER=$(bitcoin-cli getblockheader "$HASH")
+  VERSION=$(echo "$HEADER" | jq .version)
+  SIGNAL=$(((VERSION & 3758096388) == 536870916))
+  case $SIGNAL in
+    (1) echo -n "🟩";;
+    (0) echo -n "🟥";;
+  esac
+done
+echo
