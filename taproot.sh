5b01febcd4c37ef47779bfa10427dbe7087aa4c1
curly60e <55191248+curly60e@users.noreply.github.com>
2021-05-14 11:25:35 -0300

    Update taproot.sh

--- taproot.sh
+++ taproot.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 BLOCKCHAININFO=$(bitcoin-cli getblockchaininfo)
+BLOCKS=$(echo "$BLOCKCHAININFO" | jq .blocks)
 TAPROOT=$(echo "$BLOCKCHAININFO" | jq .softforks.taproot.bip9)
 SINCE=$(echo "$TAPROOT" | jq .since)
+PERIOD=$(echo "$TAPROOT" | jq .statistics.period)
+BLOCKS=$(echo "$BLOCKCHAININFO" | jq .blocks)
+PERIOD_COUNT=$(((BLOCKS - SINCE) / PERIOD))
+SINCE=$((SINCE + (PERIOD * PERIOD_COUNT)))
 ELAPSED=$(echo "$TAPROOT" | jq .statistics.elapsed)
- for BLOCK in $(seq "$SINCE" $((SINCE + ELAPSED - 1))); do
+for BLOCK in $(seq "$SINCE" $((SINCE + ELAPSED - 1))); do
   HASH=$(bitcoin-cli getblockhash "$BLOCK")
   HEADER=$(bitcoin-cli getblockheader "$HASH")
   VERSION=$(echo "$HEADER" | jq .version)
   SIGNAL=$(((VERSION & 3758096388) == 536870916))
   case $SIGNAL in
-    (1) echo -n "🌱";;
-    (0) echo -n "🆘";;
+    (1) echo -n "✅";;
+    (0) echo -n "❌";;
   esac
 done
 echo
+
+PERCENTAGE=`echo $BLOCKCHAININFO | jq '.softforks.taproot.bip9.statistics | .count / .elapsed * 100'`
+echo $PERCENTAGE
