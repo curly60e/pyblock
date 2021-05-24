e6fee4afae9e4ab9ecfedf4244474f933cad2a6d
curly60e <55191248+curly60e@users.noreply.github.com>
2021-05-24 11:18:00 -0300

    Update PyBlock.py

--- PyBlock.py
+++ PyBlock.py
@@ -32,7 +32,7 @@ from PIL import Image
 from robohash import Robohash
 
 
-version = "0.9.8.4"
+version = "0.9.8.5"
 
 def sysinfo():  #Cpu and memory usage
     print("    \033[0;37;40m----------------------")
