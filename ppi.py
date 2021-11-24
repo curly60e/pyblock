b74e80e874f012b326158ac327c58843da90b595
curly60e <55191248+curly60e@users.noreply.github.com>
2021-11-24 19:21:20 -0300

    Removing code

--- ppi.py
+++ ppi.py
@@ -219,9 +219,6 @@ def trustednode():
 
 def bwtConn():
     try:
-        clear()
-        blogo()
-        closed()
         conn = "curl -s https://bwt.dev/banner.txt"
         a = os.popen(conn).read()
         clear()
@@ -238,9 +235,6 @@ def bwtConn():
 
 def statsconn():
     try:
-        clear()
-        blogo()
-        closed()
         conn = """curl -s https://www.bitcoinblockhalf.com/ | html2text | grep -E "Total" -A 10  | grep -v -E "\--" | tr -d '*'"""
         a = os.popen(conn).read()
         clear()
