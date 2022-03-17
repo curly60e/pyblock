3417f8df395d4e06faec906bd5c9d08d72ebb0e2
curly60e <55191248+curly60e@users.noreply.github.com>
2022-03-16 21:12:09 -0300

    Update PyBlock.py

--- pybitblock/PyBlock.py
+++ pybitblock/PyBlock.py
@@ -6551,7 +6551,7 @@ def introINIT():
 settings = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
 settingsClock = {"gradient":"", "colorA":"green", "colorB":"yellow"}
 while True: # Loop
-    #try:
+    try:
         path = {"ip_port":"", "rpcuser":"", "rpcpass":"", "bitcoincli":""}
         if os.path.isfile('config/bclock.conf') or os.path.isfile('config/blnclock.conf'): # Check if the file 'bclock.conf' is in the same folder
             pathv = pickle.load(open("config/bclock.conf", "rb")) # Load the file 'bclock.conf'
@@ -6564,6 +6564,6 @@ while True: # Loop
             introINIT()
         else:
             menuSelection()
-    #except:
-    #    print("\n")
-    #    sys.exit(101)
+    except:
+        print("\n")
+        sys.exit(101)
