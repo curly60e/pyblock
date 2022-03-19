d6869f4cb39631f4ccd8a73166d99c6fb24bb80c
curly60e <55191248+curly60e@users.noreply.github.com>
2022-03-19 16:57:42 -0300

    Add files via upload

--- pybitblock/PyBlock.py
+++ pybitblock/PyBlock.py
@@ -6834,7 +6834,7 @@ def introINIT():
 settings = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
 settingsClock = {"gradient":"", "colorA":"green", "colorB":"yellow"}
 while True: # Loop
-    #try:
+    try:
         path = {"ip_port":"", "rpcuser":"", "rpcpass":"", "bitcoincli":""}
         if os.path.isfile('config/bclock.conf') or os.path.isfile('config/blnclock.conf'): # Check if the file 'bclock.conf' is in the same folder
             pathv = pickle.load(open("config/bclock.conf", "rb")) # Load the file 'bclock.conf'
@@ -6847,6 +6847,6 @@ while True: # Loop
             introINIT()
         else:
             menuSelection()
-    #except:
-    #    print("\n")
-    #    sys.exit(101)
+    except:
+        print("\n")
+        sys.exit(101)
