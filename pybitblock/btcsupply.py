bb7b133679c7a95e3ffa5ca3e4aa6274e7a889f4
Sourcery AI <>
2022-03-19 02:11:22 +0000

    'Refactored by Sourcery'

--- pybitblock/btcsupply.py
+++ pybitblock/btcsupply.py
@@ -5,16 +5,15 @@
 def btcSupplyAtBlock(b):
     if b >= 33 * 210000:
         return 20999999.9769
-    else:
-        reward = 50e8
-        supply = 0
-        y = 210000  # reward changes all y blocks
-        while b > y - 1:
-            supply = supply + y * reward
-            reward = int(reward / 2.0)
-            b = b - y
-        supply = supply + b * reward
-        return (supply + reward) / 1e8
+    reward = 50e8
+    supply = 0
+    y = 210000  # reward changes all y blocks
+    while b > y - 1:
+        supply = supply + y * reward
+        reward = int(reward / 2.0)
+        b = b - y
+    supply = supply + b * reward
+    return (supply + reward) / 1e8
 
 
 if __name__ == "__main__":
