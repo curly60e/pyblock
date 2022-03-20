cc7b27ba8b08d89692ff8a7c3279ae353c2cb41c
curly60e <55191248+curly60e@users.noreply.github.com>
2022-03-20 16:58:23 -0300

    Add files via upload

--- pybitblock/mempoolclock.py
+++ pybitblock/mempoolclock.py
@@ -38,31 +38,88 @@ def pathexec():
     path = pathv # Copy the variable pathv to 'path'
 
 def counttxs():
+    if os.path.isfile('config/pyblocksettingsClock.conf') or os.path.isfile('config/pyblocksettingsClock.conf'): # Check if the file 'bclock.conf' is in the same folder
+        settingsv = pickle.load(open("config/pyblocksettingsClock.conf", "rb")) # Load the file 'bclock.conf'
+        settingsClock = settingsv # Copy the variable pathv to 'path'
+    else:
+        settingsClock = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
+        pickle.dump(settingsClock, open("config/pyblocksettingsClock.conf", "wb"))
+    bitcoinclient = f'{path["bitcoincli"]} getblockcount'
+    block = os.popen(str(bitcoinclient)).read() # 'getblockcount' convert to string
+    b = block
+    a = b
+    pathexec()
+    clear()
+    getrawmempool = " getrawmempool"
+    gna = os.popen(path['bitcoincli'] + getrawmempool)
+    gnaa = gna.read()
+    gna1 = str(gnaa)
+    d = json.loads(gna1)
+    e = len(d)
+    n = e / 10
+    outputtxs = render(str(e) + " txs", colors=[settingsClock['colorA'], settingsClock['colorB']], align='center', font='tiny')
+    print("\x1b[?25l" + outputtxs)
+    shq = int(n)
+    ss = str(rectangle(shq))
+    qq = ss.replace("None","")
+    print(f"\033[A{qq}\033[A")
+    #output = render(str(b), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
+    #print("\033[0;37;40m\x1b[?25l" + output)
     while True:
-        try:
-            pathexec()
+        clear()
+        x = a
+        bitcoinclient = f'{path["bitcoincli"]} getblockcount'
+        block = os.popen(str(bitcoinclient)).read() # 'getblockcount' convert to string
+        b = block
+        pathexec()
+        clear()
+        getrawmempool = " getrawmempool"
+        gna = os.popen(path['bitcoincli'] + getrawmempool)
+        gnaa = gna.read()
+        gna1 = str(gnaa)
+        d = json.loads(gna1)
+        e = len(d)
+        n = e / 10
+        outputtxs = render(str(e) + " txs", colors=[settingsClock['colorA'], settingsClock['colorB']], align='center', font='tiny')
+        print("\x1b[?25l" + outputtxs)
+        shq = int(n)
+        ss = str(rectangle(shq))
+        qq = ss.replace("None","")
+        print(f"\033[A{qq}\033[A")
+        if b > a:
             clear()
-            getrawmempool = " getrawmempool"
-            gna = os.popen(path['bitcoincli'] + getrawmempool)
-            gnaa = gna.read()
-            gna1 = str(gnaa)
-            d = json.loads(gna1)
-            e = len(d)
-            outputtxs = render(
-                f'{e} unconfirmed txs',
-                colors=[settingsClock['colorA'], settingsClock['colorB']],
-                align='center',
-                font='tiny',
-            )
-
+            close()
+            output = render(str(b), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
+            print("\a\x1b[?25l" + output)
+            bitcoinclient = f'{path["bitcoincli"]} getbestblockhash'
+            bb = os.popen(str(bitcoinclient)).read()
+            ll = bb
+            bitcoinclientgetblock = f'{path["bitcoincli"]} getblock {ll}'
+            qq = os.popen(bitcoinclientgetblock).read()
+            yy = json.loads(qq)
+            mm = yy
+            outputsize = render(str(mm['size']) + " bytes", colors=[settingsClock['colorA'], settingsClock['colorB']], align='center', font='tiny')
+            print("\x1b[?25l" + outputsize)
+            outputtxs = render(str(mm['nTx']) + " txs", colors=[settingsClock['colorA'], settingsClock['colorB']], align='center', font='tiny')
             print("\x1b[?25l" + outputtxs)
-            shq = e // 10
+            sh = int(mm['nTx']) / 4
+            shq = int(sh)
             ss = str(rectangle(shq))
-            qq = ss.replace("None","")
-            print(f"\033[A{qq}\033[A")
+            print(ss.replace("None",""))
             t.sleep(10)
-        except:
-            break
+            txs = str(mm['nTx'])
+            if txs == "1":
+                try:
+                    p = subprocess.Popen(['curl', 'https://poptart.spinda.net'])
+                    p.wait(5)
+                except subprocess.TimeoutExpired:
+                    p.kill()
+            print("\033[0;37;40m\x1b[?25l")
+            clear()
+            close()
+            a = b
+            output = render(str(b), colors=[settingsClock['colorA'], settingsClock['colorB']], align='center')
+            print("\x1b[?25l" + output)
 
 settings = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
 settingsClock = {"gradient":"", "colorA":"green", "colorB":"yellow"}
