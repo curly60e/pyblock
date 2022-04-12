9e18c0bccaec2ed5a6cb36dfd2737c053aa2a0ca
Sourcery AI <>
2022-04-12 21:04:45 +0000

    'Refactored by Sourcery'

--- pybitblock/PyBlock.py
+++ pybitblock/PyBlock.py
@@ -3788,7 +3788,7 @@ def colorsSelectBackOnchainONLY():
         cert_path = lndconnectload["tls"]
         macaroon = codecs.encode(open(lndconnectload["macaroon"], 'rb').read(), 'hex')
         headers = {'Grpc-Metadata-macaroon': macaroon}
-        url = 'https://{}/v1/getinfo'.format(lndconnectload["ip_port"])
+        url = f'https://{lndconnectload["ip_port"]}/v1/getinfo'
         r = requests.get(url, headers=headers, verify=cert_path)
         alias = r.json()
     print("""\t\t
