ed04a7dbf9e7509ee2c029773f80f9394be4f79a
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2022-09-03 18:54:43 +0200

    Update spvblock.py

--- pybitblock/SPV/spvblock.py
+++ pybitblock/SPV/spvblock.py
@@ -33,7 +33,7 @@ from lnpay_py.wallet import LNPayWallet
 from pycoingecko import CoinGeckoAPI
 
 
-version = "2.0.4-alpha"
+version = "2.0.4"
 
 settings = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
 settingsClock = {"gradient":"", "colorA":"green", "colorB":"yellow"}
