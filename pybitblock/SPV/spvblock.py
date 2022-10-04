beff6a348adf37228f3de03f5a57514371f05876
Sourcery AI <>
2022-10-04 18:30:53 +0000

    'Refactored by Sourcery'

--- pybitblock/SPV/spvblock.py
+++ pybitblock/SPV/spvblock.py
@@ -3434,9 +3434,7 @@ def runthenumbers():
         blogo()
         closed()
         output = render("total amount", colors=['yellow'], align='left', font='tiny')
-        outputT = render(
-            a + " sats", colors=['green'], align='left', font='tiny'
-        )
+        outputT = render(f"{a} sats", colors=['green'], align='left', font='tiny')
         print(output)
         print(outputT)
         input("\a\nContinue...")
