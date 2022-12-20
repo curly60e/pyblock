ac00b20b309e1ffda09b22ad5070412c1d79bde4
curly60e <55191248+curly60e@users.noreply.github.com>
2022-12-20 08:06:22 -0300

    Update spvblock.py

--- pybitblock/SPV/spvblock.py
+++ pybitblock/SPV/spvblock.py
@@ -33,7 +33,7 @@ from lnpay_py.wallet import LNPayWallet
 from pycoingecko import CoinGeckoAPI
 
 
-version = "2.0.7"
+version = "2.0.8"
 
 settings = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
 settingsClock = {"gradient":"", "colorA":"green", "colorB":"yellow"}
