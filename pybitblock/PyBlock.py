f13a23f577752ae6fa85a7413c218ab756640b38
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2023-01-15 20:33:11 +0100

    Update PyBlock.py

--- pybitblock/PyBlock.py
+++ pybitblock/PyBlock.py
@@ -1287,9 +1287,7 @@ def callGitBija():
         os.system(git)
     os.system("cd bija && docker-compose up")
     input("\a\nYou can now access Bija at http://localhost:5000")
-   except:
-        menuSelection()        
-
+    
 #---------------------------------ColdCore-----------------------------------------
 def callColdCore():
     clear()
