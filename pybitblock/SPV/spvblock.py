588a020e99953c6397cec9c191e966a23571db27
curly60e <55191248+curly60e@users.noreply.github.com>
2023-09-20 08:17:56 -0300

    Update spvblock.py

--- pybitblock/SPV/spvblock.py
+++ pybitblock/SPV/spvblock.py
@@ -39,7 +39,7 @@ from embit.wordlists.bip39 import WORDLIST
 from io import StringIO
 
 
-version = "2.2"
+version = "2.2.1"
 
 settings = {"gradient":"", "design":"block", "colorA":"green", "colorB":"yellow"}
 settingsClock = {"gradient":"", "colorA":"green", "colorB":"yellow"}
