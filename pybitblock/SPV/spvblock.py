73ff2cf166b21a3c523dd0676b7757e884507692
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2023-09-22 20:54:38 +0200

    Update spvblock.py

--- pybitblock/SPV/spvblock.py
+++ pybitblock/SPV/spvblock.py
@@ -660,18 +660,13 @@ def mtConn(): # here we convert the result of the command 'getblockcount' on a r
 
 def mtclock():
     try:
-        conn = """curl -s https://bitcoinexplorer.org/api/price/sats | jq -C '.[]' | head -n 1 """
-        a = os.popen(conn).read()
         clear()
         blogo()
         closed()
         output = render("Moscow Time", colors=['yellow'], align='center', font='tiny')
-        outputT = render(f"{a} O'Clock", colors=['green'], align='center', font='tiny')
+        outputT = render("O'Clock", colors=['green'], align='center', font='tiny')
         print(output)
         print(outputT)
-        clear()
-        close()
-        mtConn()
     except:
         pass
 #-----------------------------END MT--------------------------------
