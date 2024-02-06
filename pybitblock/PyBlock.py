97bb9cbc4e2029dedc7ea367e62e68dbf6f494f0
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2024-02-06 01:32:44 +0100

    Update PyBlock.py

--- pybitblock/PyBlock.py
+++ pybitblock/PyBlock.py
@@ -7514,7 +7514,7 @@ def fullbtclnd():
                 lndconnectload["ln"] = input("Insert the Path to Lncli. Normally you just need to type 𝙡𝙣𝙘𝙡𝙞: ")
                 pickle.dump(lndconnectload, open("config/blndconnect.conf", "wb")) # Save the file 'bclock.conf'
             if yesno in ["NO", "no", "nO", "No"]:
-                menuSelection()
+                fullbtc()
 
 
 def introINIT():
