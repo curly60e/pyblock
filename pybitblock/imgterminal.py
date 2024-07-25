7436ebf674df77d07c64c48c4a1f22a8922873d2
curly60e <55191248+curly60e@users.noreply.github.com>
2024-07-25 16:08:16 -0300

    Update imgterminal.py

--- pybitblock/imgterminal.py
+++ pybitblock/imgterminal.py
@@ -1,29 +1,39 @@
 import shutil
+import os
 from PIL import Image as PILImage
 from term_image.image import from_file
 
-# Ruta al archivo de imagen
-image_path = "bitaxe.jpg"
+def set_terminal_background(color="black"):
+    if color == "black":
+        os.system('printf "\033[40m"')  # Secuencia de escape ANSI para fondo negro
+    elif color == "reset":
+        os.system('printf "\033[49m"')  # Secuencia de escape ANSI para restaurar el fondo
 
-# Cargar la imagen usando PIL y redimensionarla
-pil_image = PILImage.open(image_path)
 
-# Obtener el tamaño de la terminal
-terminal_size = shutil.get_terminal_size()
+def createimagebitaxe():
+    # Ruta al archivo de imagen
+    image_path = "bitaxe.jpg"
 
-# Ajustar el tamaño de la imagen según el tamaño de la terminal
-# Restar algunos caracteres para asegurarse de que encaje bien
-max_width = (terminal_size.columns - 4) * 2  # Ajustar el factor según sea necesario
-max_height = (terminal_size.lines - 4) * 4  # Ajustar el factor según sea necesario
+    # Cargar la imagen usando PIL y redimensionarla
+    pil_image = PILImage.open(image_path)
 
-# Redimensionar la imagen manteniendo la proporción
-pil_image.thumbnail((max_width, max_height))
+    # Obtener el tamaño de la terminal
+    terminal_size = shutil.get_terminal_size()
 
-# Guardar la imagen redimensionada temporalmente
-temp_image_path = "resized_image.png"
-pil_image.save(temp_image_path)
+    # Ajustar el tamaño de la imagen según el tamaño de la terminal
+    # Restar algunos caracteres para asegurarse de que encaje bien
+    max_width = (terminal_size.columns - 4) * 2  # Ajustar el factor según sea necesario
+    max_height = (terminal_size.lines - 4) * 4  # Ajustar el factor según sea necesario
 
-# Cargar la imagen redimensionada usando term-image
-image = from_file(temp_image_path)
+    # Redimensionar la imagen manteniendo la proporción
+    pil_image.thumbnail((max_width, max_height))
 
-image.draw()
+    # Guardar la imagen redimensionada temporalmente
+    temp_image_path = "resized_image.png"
+    pil_image.save(temp_image_path)
+
+    # Cargar la imagen redimensionada usando term-image
+    image = from_file(temp_image_path)
+    # Envolver el comando draw en secuencias de escape para mantener el fondo negro
+    image.draw()
+
