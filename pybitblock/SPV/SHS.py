9e84563e79980346bc2be9634db1927b8d2c2e07
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2024-08-15 00:51:17 +0200

    Update SHS.py

--- pybitblock/SPV/SHS.py
+++ pybitblock/SPV/SHS.py
@@ -15,7 +15,7 @@ host    = 'pool.pyblock.xyz'
 port    = 3333
 
 def main():
-    print("address:{} nonce:{}".format(address,nonce))
+    print("\nSatoshi:{}\n\nNonce:{}\n".format(address,nonce))
 
     sock    = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host,port))
@@ -39,14 +39,14 @@ def main():
         = responses[0]['params']
    
     target = (nbits[2:]+'00'*(int(nbits[:2],16) - 3)).zfill(64)
-    print('nbits:{} target:{}\n'.format(nbits,target))
+    print('\nNbits:{}\n\nTarget:{}\n'.format(nbits,target))
    
     extranonce2 = hex(random.randint(0,2**32-1))[2:].zfill(2*extranonce2_size)
    
     coinbase = coinb1 + extranonce1 + extranonce2 + coinb2
     coinbase_hash_bin = hashlib.sha256(hashlib.sha256(binascii.unhexlify(coinbase)).digest()).digest()
    
-    print('coinbase:\n{}\n\ncoinbase hash:{}\n'.format(coinbase,binascii.hexlify(coinbase_hash_bin)))
+    print('Coinbase:\n{}\n\nCoinbase Hash:{}\n'.format(coinbase,binascii.hexlify(coinbase_hash_bin)))
     merkle_root = coinbase_hash_bin
     for h in merkle_branch:
         merkle_root = hashlib.sha256(hashlib.sha256(merkle_root + binascii.unhexlify(h)).digest()).digest()
@@ -55,7 +55,7 @@ def main():
    
     merkle_root = ''.join([merkle_root[i]+merkle_root[i+1] for i in range(0,len(merkle_root),2)][::-1])
    
-    print('merkle_root:{}\n'.format(merkle_root))
+    print('Merkle Root:{}\n'.format(merkle_root))
    
     def noncework():
         nonce   = hex(random.randint(0,2**32-1))[2:].zfill(8)
@@ -64,19 +64,19 @@ def main():
        
         hash = hashlib.sha256(hashlib.sha256(binascii.unhexlify(blockheader)).digest()).digest()
         hash = binascii.hexlify(hash).decode()
-        if(hash[:5] == '00000'): print('hash: {}'.format(hash))
+        if(hash[:5] == '00000'): print('Hash: {}'.format(hash))
         if hash < target :
-            print('success!!')
-            print('hash: {}'.format(hash))
+            print('\nSuccess!!\n')
+            print('\nHash: {}\n'.format(hash))
             payload = bytes('{"params": ["'+address+'", "'+job_id+'", "'+extranonce2 \
                 +'", "'+ntime+'", "'+nonce+'"], "id": 1, "method": "mining.submit"}\n', 'utf-8')
             sock.sendall(payload)
             print(sock.recv(1024))
-            input("Press Enter to continue...")
+            input("\nPress Enter to continue...")
    
     for k in range(33333333):
         noncework()
-    print("Symbolic-Hash-Satoshi Finished with 33M Attempts. Trying Again...")
+    print("\nSymbolic-Hash-Satoshi Finished with 33M Attempts.\n\nTrying Again...\n")
     sock.close()
     main()
 
