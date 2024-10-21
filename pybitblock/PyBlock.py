b98cba242b0d133f55820ad4e0300435c98e9120
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2024-10-22 00:50:59 +0200

    Update PyBlock.py

--- pybitblock/PyBlock.py
+++ pybitblock/PyBlock.py
@@ -6983,7 +6983,7 @@ def bitcoincoremenuLOCALcontrolA(bcore):
     elif bcore in ["CM", "cm"]:
         CoreMiner()
     elif bcore in ["ONM", "onm"]:
-        OwnNodeMinerONCHAIN()
+        OwnNodeMinerControlONCHAIN()
 
 def bitcoincoremenuLOCALcontrolAOnchainONLY(bcore):
     if bcore in ["A", "a"]:
@@ -7084,7 +7084,7 @@ def bitcoincoremenuLOCALcontrolAOnchainONLY(bcore):
     elif bcore in ["CM", "cm"]:
         CoreMiner()
     elif bcore in ["ONM", "onm"]:
-        OwnNodeMinerONCHAIN()
+        OwnNodeMinerControlONCHAIN()
 
 def walletmenuLOCALcontrolAOnchainONLY(walletmnu):
     if walletmnu in ["A", "a"]:
