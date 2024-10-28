8ea8fba0709821296eb81401519c6ab156d767ab
Satoshi Nakamoto <65907137+SatoshiNakamotoBitcoin@users.noreply.github.com>
2024-10-29 00:00:32 +0100

    Update PyVanityGenerator.py

--- pybitblock/SPV/PyVanityGenerator.py
+++ pybitblock/SPV/PyVanityGenerator.py
@@ -8,8 +8,5 @@ signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
 
 def callback(address):
     return address.startswith(b'1X')
-          #address.slice(1).startsWith('X')
-          #address.endsWith('X')
-          #address.includes('X')
 address = VanityAddressGenerator.generate_one(callback=callback)
 print("Address:\t{address.address}\nPrivate key:\t{address.private_key}".format(address=address))
